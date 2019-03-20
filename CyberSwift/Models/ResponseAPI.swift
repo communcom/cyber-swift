b5628890cae3ee02e6f6a5e52c9d6d8f91a25ce4
SerhiiM <msm72@mail.ru>
2019-03-20 20:38:00 +0200

    Modify 'ResponseAPI'.

--- CyberSwift/Models/ResponseAPI.swift
+++ CyberSwift/Models/ResponseAPI.swift
@@ -197,7 +197,7 @@ public struct ResponseAPIContentGetPostContentMetadataEmbed: Decodable {
     // MARK: - In work API `content.getFeed`
     public let url: String?
     public let result: ResponseAPIContentGetPostContentMetadataEmbedResult?
-    public let id: String // Conflicted
+    public let id: Conflicted
     public let html: String?
     public let type: String?
 }
