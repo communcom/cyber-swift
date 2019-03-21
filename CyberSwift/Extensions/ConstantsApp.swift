ce6513e55f2ea8e43482b8d9b48eb3a584b50625
SerhiiM <msm72@mail.ru>
2019-03-21 08:17:28 +0200

    Modify 'WebSocketManager': store secret key value.

--- CyberSwift/Extensions/ConstantsApp.swift
+++ CyberSwift/Extensions/ConstantsApp.swift
@@ -56,4 +56,8 @@ public struct Config {
             return ReachabilityManager.connection()
         }
     }
+    
+    
+    /// Keys
+    static let secretKey: String                        =   "secretKey"
 }
