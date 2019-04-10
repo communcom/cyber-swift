3c03c319dd8c573fc6c02a3a09a0258e6742a275
SerhiiM <msm72@mail.ru>
2019-04-10 13:55:50 +0300

    Modify 'PhoneCode'.

--- CyberSwift/Models/PhoneCode.swift
+++ CyberSwift/Models/PhoneCode.swift
@@ -39,43 +39,50 @@ public struct Override: Encodable {
 
 public class PhoneCode {
     // MARK: - Properties
-    public class UnitedStates: County {
-    }
+    class UnitedStates: County {}
+    class Russia: County {}
+    class Kazakhstan: County {}
+    class Lithuania: County {}
+    class Belarus: County {}
+    class Ukraine: County {}
+    class Afghanistan: County {}
+    class Albania: County {}
+    class Algeria: County {}
+    class AmericanSamoa: County {}
+    class Andorra: County {}
+    class Angola: County {}
+    class Anguilla: County {}
+    class Antigua: County {}
+    class Argentina: County {}
+    class Armenia: County {}
+    class Aruba: County {}
+    class Australia: County {}
+    class Austria: County {}
+    class Azerbaijan: County {}
+    class Bahrain: County {}
+    class Bangladesh: County {}
+    class Barbados: County {}
+    class Belgium: County {}
+    class Belize: County {}
+    class Benin: County {}
+    class Bermuda: County {}
+    class Bhutan: County {}
+    class Bolivia: County {}
+    class BonaireSintEustatiusSaba: County {}
+    class BosniaHerzegovina: County {}
+    class Botswana: County {}
+    class Brazil: County {}
+    class BritishIndianOceanTerritory: County {}
+    class BritishVirginIslands: County {}
+    class Brunei: County {}
+    class Bulgaria: County {}
+    class BurkinaFaso: County {}
+    class Burundi: County {}
     
-    public class Russia: County {
-    }
 
-    public class Kazakhstan: County {
-    }
     
-    public class Lithuania: County {
-    }
-    
-    public class Belarus: County {
-    }
 
-    public class Ukraine: County {
-    }
 
-    public class Afghanistan: County {
-    }
-    
-    public class Albania: County {
-    }
-
-    public class Algeria: County {
-    }
-
-    public class AmericanSamoa: County {
-    }
-    
-    public class Andorra: County {
-    }
-    
-    public class Angola: County {
-    }
-
-    
     
     
     // MARK: - Functions
@@ -91,7 +98,841 @@ public class PhoneCode {
                  Algeria(codeValue: 213, labelValue: "Algeria", flagURLString: "https://cdn.countryflags.com/thumbs/algeria/flag-800.png"),
                  AmericanSamoa(codeValue: 1, labelValue: "American Samoa", flagURLString: "https://cdn.countryflags.com/thumbs/samoa/flag-800.png"),
                  Andorra(codeValue: 376, labelValue: "Andorra", flagURLString: "https://cdn.countryflags.com/thumbs/andorra/flag-800.png"),
-                 Angola(codeValue: 244, labelValue: "Angola", flagURLString: "https://cdn.countryflags.com/thumbs/angola/flag-800.png")
+                 Angola(codeValue: 244, labelValue: "Angola", flagURLString: "https://cdn.countryflags.com/thumbs/angola/flag-800.png"),
+                 Anguilla(codeValue: 1, labelValue: "Anguilla", flagURLString: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Flag_of_Anguilla.svg/2880px-Flag_of_Anguilla.svg.png"),
+            Antigua(codeValue: 1, labelValue: "Antigua", flagURLString: "https://cdn.countryflags.com/thumbs/antigua-and-barbuda/flag-800.png"),
+            Argentina(codeValue: 54, labelValue: "Argentina", flagURLString: "https://cdn.countryflags.com/thumbs/argentina/flag-800.png"),
+            Armenia(codeValue: 374, labelValue: "Armenia", flagURLString: "https://cdn.countryflags.com/thumbs/armenia/flag-800.png"),
+            Aruba(codeValue: 297, labelValue: "Aruba", flagURLString: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Flag_of_Aruba.svg/2560px-Flag_of_Aruba.svg.png"),
+            Australia(codeValue: 61, labelValue: "Australia", flagURLString: "https://cdn.countryflags.com/thumbs/australia/flag-800.png"),
+            Austria(codeValue: 43, labelValue: "Austria", flagURLString: "https://cdn.countryflags.com/thumbs/austria/flag-800.png"),
+            Azerbaijan(codeValue: 994, labelValue: "Azerbaijan", flagURLString: "https://cdn.countryflags.com/thumbs/azerbaijan/flag-800.png"),
+            Bahrain(codeValue: 973, labelValue: "Bahrain", flagURLString: "https://cdn.countryflags.com/thumbs/bahrain/flag-800.png"),
+            Bangladesh(codeValue: 880, labelValue: "Bangladesh", flagURLString: "https://cdn.countryflags.com/thumbs/bangladesh/flag-800.png"),
+            Barbados(codeValue: 1, labelValue: "Barbados", flagURLString: "https://cdn.countryflags.com/thumbs/barbados/flag-800.png"),
+            Belgium(codeValue: 32, labelValue: "Belgium", flagURLString: "https://cdn.countryflags.com/thumbs/belgium/flag-800.png"),
+            Belize(codeValue: 501, labelValue: "Belize", flagURLString: "https://cdn.countryflags.com/thumbs/belize/flag-800.png"),
+            Benin(codeValue: 229, labelValue: "Benin", flagURLString: "https://cdn.countryflags.com/thumbs/benin/flag-800.png"),
+            Bermuda(codeValue: 1, labelValue: "Bermuda", flagURLString: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Flag_of_Bermuda.svg/2880px-Flag_of_Bermuda.svg.png"),
+            Bhutan(codeValue: 975, labelValue: "Bhutan", flagURLString: "https://cdn.countryflags.com/thumbs/bhutan/flag-800.png"),
+            Bolivia(codeValue: 591, labelValue: "Bolivia", flagURLString: "https://cdn.countryflags.com/thumbs/bolivia/flag-800.png"),
+            BonaireSintEustatiusSaba(codeValue: 599, labelValue: "Bonaire, Sint Eustatius and Saba", flagURLString: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Flag_of_the_Netherlands.svg/2560px-Flag_of_the_Netherlands.svg.png"),
+            BosniaHerzegovina(codeValue: 387, labelValue: "Bosnia and Herzegovina", flagURLString: "https://cdn.countryflags.com/thumbs/bosnia-and-herzegovina/flag-800.png"),
+            Botswana(codeValue: 267, labelValue: "Botswana", flagURLString: "https://cdn.countryflags.com/thumbs/botswana/flag-800.png"),
+            Brazil(codeValue: 55, labelValue: "Brazil", flagURLString: "https://cdn.countryflags.com/thumbs/brazil/flag-800.png"),
+            BritishIndianOceanTerritory(codeValue: 246, labelValue: "British Indian Ocean Territory", flagURLString: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Flag_of_the_British_Indian_Ocean_Territory.svg/2880px-Flag_of_the_British_Indian_Ocean_Territory.svg.png"),
+            BritishVirginIslands(codeValue: 1, labelValue: "British Virgin Islands", flagURLString: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Flag_of_the_British_Virgin_Islands.svg/2880px-Flag_of_the_British_Virgin_Islands.svg.png"),
+            Brunei(codeValue: 673, labelValue: "Brunei", flagURLString: "https://cdn.countryflags.com/thumbs/brunei/flag-800.png"),
+            Bulgaria(codeValue: 359, labelValue: "Bulgaria", flagURLString: "https://cdn.countryflags.com/thumbs/bulgaria/flag-800.png"),
+            BurkinaFaso(codeValue: 226, labelValue: "Burkina Faso", flagURLString: "https://cdn.countryflags.com/thumbs/burkina-faso/flag-800.png"),
+            Burundi(codeValue: 257, labelValue: "Burundi", flagURLString: "https://cdn.countryflags.com/thumbs/burundi/flag-800.png")
+            
+
+    
         ]
     }
+
+    /*
+    {
+    "code": 855,
+    "label": "Cambodia"
+    },
+    {
+    "code": 237,
+    "label": "Cameroon"
+    },
+    {
+    "code": 1,
+    "label": "Canada"
+    },
+    {
+    "code": 238,
+    "label": "Cape Verde"
+    },
+    {
+    "code": 1,
+    "label": "Cayman Islands"
+    },
+    {
+    "code": 236,
+    "label": "Central African Republic"
+    },
+    {
+    "code": 235,
+    "label": "Chad"
+    },
+    {
+    "code": 56,
+    "label": "Chile"
+    },
+    {
+    "code": 86,
+    "label": "China"
+    },
+    {
+    "code": 57,
+    "label": "Colombia"
+    },
+    {
+    "code": 269,
+    "label": "Comoros"
+    },
+    {
+    "code": 682,
+    "label": "Cook Islands"
+    },
+    {
+    "code": 506,
+    "label": "Costa Rica"
+    },
+    {
+    "code": 225,
+    "label": "Côte d'Ivoire"
+    },
+    {
+    "code": 385,
+    "label": "Croatia"
+    },
+    {
+    "code": 53,
+    "label": "Cuba"
+    },
+    {
+    "code": 599,
+    "label": "Curaçao"
+    },
+    {
+    "code": 357,
+    "label": "Cyprus"
+    },
+    {
+    "code": 420,
+    "label": "Czech Republic"
+    },
+    {
+    "code": 243,
+    "label": "Democratic Republic of the Congo"
+    },
+    {
+    "code": 45,
+    "label": "Denmark"
+    },
+    {
+    "code": 253,
+    "label": "Djibouti"
+    },
+    {
+    "code": 1,
+    "label": "Dominica"
+    },
+    {
+    "code": 1,
+    "label": "Dominican Republic"
+    },
+    {
+    "code": 593,
+    "label": "Ecuador"
+    },
+    {
+    "code": 20,
+    "label": "Egypt"
+    },
+    {
+    "code": 503,
+    "label": "El Salvador"
+    },
+    {
+    "code": 240,
+    "label": "Equatorial Guinea"
+    },
+    {
+    "code": 291,
+    "label": "Eritrea"
+    },
+    {
+    "code": 372,
+    "label": "Estonia"
+    },
+    {
+    "code": 251,
+    "label": "Ethiopia"
+    },
+    {
+    "code": 500,
+    "label": "Falkland Islands"
+    },
+    {
+    "code": 298,
+    "label": "Faroe Islands"
+    },
+    {
+    "code": 691,
+    "label": "Federated States of Micronesia"
+    },
+    {
+    "code": 679,
+    "label": "Fiji"
+    },
+    {
+    "code": 358,
+    "label": "Finland"
+    },
+    {
+    "code": 33,
+    "label": "France"
+    },
+    {
+    "code": 594,
+    "label": "French Guiana"
+    },
+    {
+    "code": 689,
+    "label": "French Polynesia"
+    },
+    {
+    "code": 241,
+    "label": "Gabon"
+    },
+    {
+    "code": 995,
+    "label": "Georgia"
+    },
+    {
+    "code": 49,
+    "label": "Germany"
+    },
+    {
+    "code": 233,
+    "label": "Ghana"
+    },
+    {
+    "code": 350,
+    "label": "Gibraltar"
+    },
+    {
+    "code": 30,
+    "label": "Greece"
+    },
+    {
+    "code": 299,
+    "label": "Greenland"
+    },
+    {
+    "code": 1,
+    "label": "Grenada"
+    },
+    {
+    "code": 590,
+    "label": "Guadeloupe"
+    },
+    {
+    "code": 1,
+    "label": "Guam"
+    },
+    {
+    "code": 502,
+    "label": "Guatemala"
+    },
+    {
+    "code": 44,
+    "label": "Guernsey"
+    },
+    {
+    "code": 224,
+    "label": "Guinea"
+    },
+    {
+    "code": 245,
+    "label": "Guinea-Bissau"
+    },
+    {
+    "code": 592,
+    "label": "Guyana"
+    },
+    {
+    "code": 509,
+    "label": "Haiti"
+    },
+    {
+    "code": 504,
+    "label": "Honduras"
+    },
+    {
+    "code": 852,
+    "label": "Hong Kong"
+    },
+    {
+    "code": 36,
+    "label": "Hungary"
+    },
+    {
+    "code": 354,
+    "label": "Iceland"
+    },
+    {
+    "code": 91,
+    "label": "India"
+    },
+    {
+    "code": 62,
+    "label": "Indonesia"
+    },
+    {
+    "code": 98,
+    "label": "Iran"
+    },
+    {
+    "code": 964,
+    "label": "Iraq"
+    },
+    {
+    "code": 353,
+    "label": "Ireland"
+    },
+    {
+    "code": 44,
+    "label": "Isle Of Man"
+    },
+    {
+    "code": 972,
+    "label": "Israel"
+    },
+    {
+    "code": 39,
+    "label": "Italy"
+    },
+    {
+    "code": 1,
+    "label": "Jamaica"
+    },
+    {
+    "code": 81,
+    "label": "Japan"
+    },
+    {
+    "code": 44,
+    "label": "Jersey"
+    },
+    {
+    "code": 962,
+    "label": "Jordan"
+    },
+    {
+    "code": 254,
+    "label": "Kenya"
+    },
+    {
+    "code": 686,
+    "label": "Kiribati"
+    },
+    {
+    "code": 383,
+    "label": "Kosovo"
+    },
+    {
+    "code": 965,
+    "label": "Kuwait"
+    },
+    {
+    "code": 996,
+    "label": "Kyrgyzstan"
+    },
+    {
+    "code": 856,
+    "label": "Laos"
+    },
+    {
+    "code": 371,
+    "label": "Latvia"
+    },
+    {
+    "code": 961,
+    "label": "Lebanon"
+    },
+    {
+    "code": 266,
+    "label": "Lesotho"
+    },
+    {
+    "code": 231,
+    "label": "Liberia"
+    },
+    {
+    "code": 218,
+    "label": "Libya"
+    },
+    {
+    "code": 423,
+    "label": "Liechtenstein"
+    },
+    {
+    "code": 352,
+    "label": "Luxembourg"
+    },
+    {
+    "code": 853,
+    "label": "Macau"
+    },
+    {
+    "code": 389,
+    "label": "Macedonia"
+    },
+    {
+    "code": 261,
+    "label": "Madagascar"
+    },
+    {
+    "code": 265,
+    "label": "Malawi"
+    },
+    {
+    "code": 60,
+    "label": "Malaysia"
+    },
+    {
+    "code": 960,
+    "label": "Maldives"
+    },
+    {
+    "code": 223,
+    "label": "Mali"
+    },
+    {
+    "code": 356,
+    "label": "Malta"
+    },
+    {
+    "code": 692,
+    "label": "Marshall Islands"
+    },
+    {
+    "code": 596,
+    "label": "Martinique"
+    },
+    {
+    "code": 222,
+    "label": "Mauritania"
+    },
+    {
+    "code": 230,
+    "label": "Mauritius"
+    },
+    {
+    "code": 262,
+    "label": "Mayotte"
+    },
+    {
+    "code": 52,
+    "label": "Mexico"
+    },
+    {
+    "code": 373,
+    "label": "Moldova"
+    },
+    {
+    "code": 377,
+    "label": "Monaco"
+    },
+    {
+    "code": 976,
+    "label": "Mongolia"
+    },
+    {
+    "code": 382,
+    "label": "Montenegro"
+    },
+    {
+    "code": 1,
+    "label": "Montserrat"
+    },
+    {
+    "code": 212,
+    "label": "Morocco"
+    },
+    {
+    "code": 258,
+    "label": "Mozambique"
+    },
+    {
+    "code": 95,
+    "label": "Myanmar"
+    },
+    {
+    "code": 264,
+    "label": "Namibia"
+    },
+    {
+    "code": 674,
+    "label": "Nauru"
+    },
+    {
+    "code": 977,
+    "label": "Nepal"
+    },
+    {
+    "code": 31,
+    "label": "Netherlands"
+    },
+    {
+    "code": 687,
+    "label": "New Caledonia"
+    },
+    {
+    "code": 64,
+    "label": "New Zealand"
+    },
+    {
+    "code": 505,
+    "label": "Nicaragua"
+    },
+    {
+    "code": 227,
+    "label": "Niger"
+    },
+    {
+    "code": 234,
+    "label": "Nigeria"
+    },
+    {
+    "code": 683,
+    "label": "Niue"
+    },
+    {
+    "code": 672,
+    "label": "Norfolk Island"
+    },
+    {
+    "code": 850,
+    "label": "North Korea"
+    },
+    {
+    "code": 1,
+    "label": "Northern Mariana Islands"
+    },
+    {
+    "code": 47,
+    "label": "Norway"
+    },
+    {
+    "code": 968,
+    "label": "Oman"
+    },
+    {
+    "code": 92,
+    "label": "Pakistan"
+    },
+    {
+    "code": 680,
+    "label": "Palau"
+    },
+    {
+    "code": 970,
+    "label": "Palestine"
+    },
+    {
+    "code": 507,
+    "label": "Panama"
+    },
+    {
+    "code": 675,
+    "label": "Papua New Guinea"
+    },
+    {
+    "code": 595,
+    "label": "Paraguay"
+    },
+    {
+    "code": 51,
+    "label": "Peru"
+    },
+    {
+    "code": 63,
+    "label": "Philippines"
+    },
+    {
+    "code": 48,
+    "label": "Poland"
+    },
+    {
+    "code": 351,
+    "label": "Portugal"
+    },
+    {
+    "code": 1,
+    "label": "Puerto Rico"
+    },
+    {
+    "code": 974,
+    "label": "Qatar"
+    },
+    {
+    "code": 242,
+    "label": "Republic of the Congo"
+    },
+    {
+    "code": 262,
+    "label": "Réunion"
+    },
+    {
+    "code": 40,
+    "label": "Romania"
+    },
+    {
+    "code": 250,
+    "label": "Rwanda"
+    },
+    {
+    "code": 590,
+    "label": "Saint Barthélemy"
+    },
+    {
+    "code": 290,
+    "label": "Saint Helena"
+    },
+    {
+    "code": 1,
+    "label": "Saint Kitts and Nevis"
+    },
+    {
+    "code": 590,
+    "label": "Saint Martin"
+    },
+    {
+    "code": 508,
+    "label": "Saint Pierre and Miquelon"
+    },
+    {
+    "code": 1,
+    "label": "Saint Vincent and the Grenadines"
+    },
+    {
+    "code": 685,
+    "label": "Samoa"
+    },
+    {
+    "code": 378,
+    "label": "San Marino"
+    },
+    {
+    "code": 239,
+    "label": "Sao Tome and Principe"
+    },
+    {
+    "code": 966,
+    "label": "Saudi Arabia"
+    },
+    {
+    "code": 221,
+    "label": "Senegal"
+    },
+    {
+    "code": 381,
+    "label": "Serbia"
+    },
+    {
+    "code": 248,
+    "label": "Seychelles"
+    },
+    {
+    "code": 232,
+    "label": "Sierra Leone"
+    },
+    {
+    "code": 65,
+    "label": "Singapore"
+    },
+    {
+    "code": 1,
+    "label": "Sint Maarten"
+    },
+    {
+    "code": 421,
+    "label": "Slovakia"
+    },
+    {
+    "code": 386,
+    "label": "Slovenia"
+    },
+    {
+    "code": 677,
+    "label": "Solomon Islands"
+    },
+    {
+    "code": 252,
+    "label": "Somalia"
+    },
+    {
+    "code": 27,
+    "label": "South Africa"
+    },
+    {
+    "code": 82,
+    "label": "South Korea"
+    },
+    {
+    "code": 211,
+    "label": "South Sudan"
+    },
+    {
+    "code": 34,
+    "label": "Spain"
+    },
+    {
+    "code": 94,
+    "label": "Sri Lanka"
+    },
+    {
+    "code": 1,
+    "label": "St. Lucia"
+    },
+    {
+    "code": 249,
+    "label": "Sudan"
+    },
+    {
+    "code": 597,
+    "label": "Suriname"
+    },
+    {
+    "code": 268,
+    "label": "Swaziland"
+    },
+    {
+    "code": 46,
+    "label": "Sweden"
+    },
+    {
+    "code": 41,
+    "label": "Switzerland"
+    },
+    {
+    "code": 963,
+    "label": "Syria"
+    },
+    {
+    "code": 886,
+    "label": "Taiwan"
+    },
+    {
+    "code": 992,
+    "label": "Tajikistan"
+    },
+    {
+    "code": 255,
+    "label": "Tanzania"
+    },
+    {
+    "code": 66,
+    "label": "Thailand"
+    },
+    {
+    "code": 1,
+    "label": "The Bahamas"
+    },
+    {
+    "code": 220,
+    "label": "The Gambia"
+    },
+    {
+    "code": 670,
+    "label": "Timor-Leste"
+    },
+    {
+    "code": 228,
+    "label": "Togo"
+    },
+    {
+    "code": 690,
+    "label": "Tokelau"
+    },
+    {
+    "code": 676,
+    "label": "Tonga"
+    },
+    {
+    "code": 1,
+    "label": "Trinidad and Tobago"
+    },
+    {
+    "code": 216,
+    "label": "Tunisia"
+    },
+    {
+    "code": 90,
+    "label": "Turkey"
+    },
+    {
+    "code": 993,
+    "label": "Turkmenistan"
+    },
+    {
+    "code": 1,
+    "label": "Turks and Caicos Islands"
+    },
+    {
+    "code": 688,
+    "label": "Tuvalu"
+    },
+    {
+    "code": 256,
+    "label": "Uganda"
+    },
+    {
+    "code": 971,
+    "label": "United Arab Emirates"
+    },
+    {
+    "code": 44,
+    "label": "United Kingdom"
+    },
+    {
+    "code": 598,
+    "label": "Uruguay"
+    },
+    {
+    "code": 1,
+    "label": "US Virgin Islands"
+    },
+    {
+    "code": 998,
+    "label": "Uzbekistan"
+    },
+    {
+    "code": 678,
+    "label": "Vanuatu"
+    },
+    {
+    "code": 39,
+    "label": "Vatican City"
+    },
+    {
+    "code": 58,
+    "label": "Venezuela"
+    },
+    {
+    "code": 84,
+    "label": "Vietnam"
+    },
+    {
+    "code": 681,
+    "label": "Wallis and Futuna"
+    },
+    {
+    "code": 212,
+    "label": "Western Sahara"
+    },
+    {
+    "code": 967,
+    "label": "Yemen"
+    },
+    {
+    "code": 260,
+    "label": "Zambia"
+    },
+    {
+    "code": 263,
+    "label": "Zimbabwe"
+    }
+ */
+    
 }
