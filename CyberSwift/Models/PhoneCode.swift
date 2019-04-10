9347871c49d9e97527d3a2d09c6f5334789c75dc
SerhiiM <msm72@mail.ru>
2019-04-10 12:48:24 +0300

    Modify 'PhoneCode' #1.

--- CyberSwift/Models/PhoneCode.swift
+++ CyberSwift/Models/PhoneCode.swift
@@ -11,7 +11,7 @@ import Foundation
 public class County: Encodable {
     public var code: UInt16
     public var label: String
-    public var flagURL: String
+    public var flagURL: URL
     public var `default`: Bool
     public var verificationPhone: String
     public var verificationOverrides: Override?
@@ -21,10 +21,10 @@ public class County: Encodable {
     }
 
 
-    init(codeValue: UInt16, labelValue: String, flagURLValue: String, defaultValue: Bool = false, verificationPhoneValue: String = "", verificationOverridesValue: Override? = nil) {
+    init(codeValue: UInt16, labelValue: String, flagURLString: String, defaultValue: Bool = false, verificationPhoneValue: String = "", verificationOverridesValue: Override? = nil) {
         self.code = codeValue
         self.label = labelValue
-        self.flagURL = flagURLValue
+        self.flagURL = URL(string: flagURLString)!
         self.default = defaultValue
         self.verificationPhone = verificationPhoneValue
         self.verificationOverrides = verificationOverridesValue
@@ -39,7 +39,7 @@ public struct Override: Encodable {
 
 public class PhoneCode {
     // MARK: - Properties
-    public var list: [Encodable] {
+    static public var list: [Encodable] {
         return self.create()
     }
     
@@ -83,19 +83,19 @@ public class PhoneCode {
     
     
     // MARK: - Functions
-    func create() -> [County] {
-        return [ UnitedStates(codeValue: 1, labelValue: "United States", flagURLValue: "original-flag-united-states-of-america"),
-                 Russia(codeValue: 7, labelValue: "Russia", flagURLValue: "original-flag-russia", defaultValue: true, verificationPhoneValue: "+7 (916) 930-63-59"),
-                 Kazakhstan(codeValue: 7, labelValue: "Kazakhstan", flagURLValue: "original-flag-kazakhstan", verificationPhoneValue: "+7 (777) 007-69-77"),
-                 Lithuania(codeValue: 370, labelValue: "Lithuania", flagURLValue: "original-flag-lithuania"),
-                 Belarus(codeValue: 375, labelValue: "Belarus", flagURLValue: "original-flag-belarus", verificationPhoneValue: "+375 (29) 230-87-70"),
-                 Ukraine(codeValue: 380, labelValue: "Ukraine", flagURLValue: "original-flag-ukraine", verificationPhoneValue: "+380 (93) 177-77-72", verificationOverridesValue: Override(startsWith: ["62", "64", "71", "72"], verificationPhone: "+7 (916) 930-63-59")),
-                 Afghanistan(codeValue: 93, labelValue: "Afghanistan", flagURLValue: "original-flag-afghanistan"),
-                 Albania(codeValue: 355, labelValue: "Albania", flagURLValue: "original-flag-albania"),
-                 Algeria(codeValue: 213, labelValue: "Algeria", flagURLValue: "original-flag-algeria"),
-                 AmericanSamoa(codeValue: 1, labelValue: "American Samoa", flagURLValue: "original-flag-samoa"),
-                 Andorra(codeValue: 376, labelValue: "Andorra", flagURLValue: "original-flag-andorra"),
-                 Angola(codeValue: 244, labelValue: "Angola", flagURLValue: "original-flag-angola")
+    class func create() -> [County] {
+        return [ UnitedStates(codeValue: 1, labelValue: "United States", flagURLString: "https://cdn.countryflags.com/thumbs/united-states-of-america/flag-800.png"),
+                 Russia(codeValue: 7, labelValue: "Russia", flagURLString: "https://cdn.countryflags.com/thumbs/russia/flag-800.png", defaultValue: true, verificationPhoneValue: "+7 (916) 930-63-59"),
+                 Kazakhstan(codeValue: 7, labelValue: "Kazakhstan", flagURLString: "https://cdn.countryflags.com/thumbs/kazakhstan/flag-800.png", verificationPhoneValue: "+7 (777) 007-69-77"),
+                 Lithuania(codeValue: 370, labelValue: "Lithuania", flagURLString: "https://cdn.countryflags.com/thumbs/lithuania/flag-800.png"),
+                 Belarus(codeValue: 375, labelValue: "Belarus", flagURLString: "https://cdn.countryflags.com/thumbs/belarus/flag-800.png", verificationPhoneValue: "+375 (29) 230-87-70"),
+                 Ukraine(codeValue: 380, labelValue: "Ukraine", flagURLString: "https://cdn.countryflags.com/thumbs/ukraine/flag-800.png", verificationPhoneValue: "+380 (93) 177-77-72", verificationOverridesValue: Override(startsWith: ["62", "64", "71", "72"], verificationPhone: "+7 (916) 930-63-59")),
+                 Afghanistan(codeValue: 93, labelValue: "Afghanistan", flagURLString: "https://cdn.countryflags.com/thumbs/afghanistan/flag-800.png"),
+                 Albania(codeValue: 355, labelValue: "Albania", flagURLString: "https://cdn.countryflags.com/thumbs/albania/flag-800.png"),
+                 Algeria(codeValue: 213, labelValue: "Algeria", flagURLString: "https://cdn.countryflags.com/thumbs/algeria/flag-800.png"),
+                 AmericanSamoa(codeValue: 1, labelValue: "American Samoa", flagURLString: "https://cdn.countryflags.com/thumbs/samoa/flag-800.png"),
+                 Andorra(codeValue: 376, labelValue: "Andorra", flagURLString: "https://cdn.countryflags.com/thumbs/andorra/flag-800.png"),
+                 Angola(codeValue: 244, labelValue: "Angola", flagURLString: "https://cdn.countryflags.com/thumbs/angola/flag-800.png")
         ]
     }
 }
