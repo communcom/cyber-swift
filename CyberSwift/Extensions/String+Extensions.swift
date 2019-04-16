cac195b463617f0af305fb72b425352287ebebb2
SerhiiM <msm72@mail.ru>
2019-04-16 18:01:10 +0300

    Modify String extension func.

--- CyberSwift/Extensions/String+Extensions.swift
+++ CyberSwift/Extensions/String+Extensions.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 extension String {
-    func randomString(length: Int) -> String {
+    public static func randomString(length: Int) -> String {
         let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
         return String((0..<length).map{ _ in letters.randomElement()! })
     }
