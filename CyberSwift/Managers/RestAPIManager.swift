22a6d088d57cb8c31f9da57b55576dbefd4a844a
SerhiiM <msm72@mail.ru>
2019-04-26 05:57:58 +0300

    Modify 'RestAPIManager'.

--- CyberSwift/Managers/RestAPIManager.swift
+++ CyberSwift/Managers/RestAPIManager.swift
@@ -379,6 +379,8 @@ public class RestAPIManager {
         
         Broadcast.instance.executeGETRequest(byContentAPIType:  methodAPIType,
                                              onResult:          { (responseAPIResult) in
+                                                Logger.log(message: "\nresponse API Result = \(responseAPIResult)\n", event: .debug)
+
                                                 guard let result = (responseAPIResult as! ResponseAPIGetOptionsResult).result else {
                                                     return errorHandling(ErrorAPI.requestFailed(message: "API \'options.get\' have error: \((responseAPIResult as! ResponseAPIGetOptionsResult).error!.message)"))
                                                 }
@@ -392,13 +394,20 @@ public class RestAPIManager {
     }
 
     // API basic `options.set`
-    public func setBasicOptions(language: String, nsfwContent: NsfwContentMode, completion: @escaping (ResponseAPISetOptionsBasic?, ErrorAPI?) -> Void) {
-        if (!Config.isNetworkAvailable) { return completion(nil, ErrorAPI.disableInternetConnection(message: nil)) }
-        
-        guard (Config.currentUser.nickName != nil) else {
-            return completion(nil, ErrorAPI.disableInternetConnection(message: nil))
+    public func setBasicOptions(language:           String,
+                                nsfwContent:        NsfwContentMode,
+                                responseHandling:   @escaping (ResponseAPISetOptionsBasic) -> Void,
+                                errorHandling:      @escaping (ErrorAPI) -> Void) {
+        // Offline mode
+        if (!Config.isNetworkAvailable) {
+            return errorHandling(ErrorAPI.disableInternetConnection(message: nil))
         }
         
+        // Check user authorize
+        guard Config.currentUser.nickName != nil else {
+            return errorHandling(ErrorAPI.invalidData(message: "Unauthorized"))
+        }
+
         let methodAPIType = MethodAPIType.setBasicOptions(nsfw: nsfwContent.rawValue, language: language)
         
         Broadcast.instance.executeGETRequest(byContentAPIType:  methodAPIType,
@@ -406,16 +415,15 @@ public class RestAPIManager {
                                                 Logger.log(message: "\nresponse API Result = \(responseAPIResult)\n", event: .debug)
                                                 
                                                 guard let result = (responseAPIResult as! ResponseAPISetOptionsBasicResult).result else {
-                                                    completion(nil, ErrorAPI.requestFailed(message: "API basic \'options.set\' have error: \((responseAPIResult as! ResponseAPISetOptionsBasicResult).error!.message)"))
-                                                    return
+                                                    return errorHandling(ErrorAPI.requestFailed(message: "API basic \'options.set\' have error: \((responseAPIResult as! ResponseAPISetOptionsBasicResult).error!.message)"))
                                                 }
                                                 
-                                                completion(result, nil)
-        }) { (errorAPI) in
-            Logger.log(message: "nresponse API Error = \(errorAPI.caseInfo.message)\n", event: .error)
-            
-            completion(nil, errorAPI)
-        }
+                                                responseHandling(result)
+        },
+                                             onError:           { (errorAPI) in
+                                                Logger.log(message: "nresponse API Error = \(errorAPI.caseInfo.message)\n", event: .error)
+                                                errorHandling(errorAPI)
+        })
     }
 
     // API push `options.set`
