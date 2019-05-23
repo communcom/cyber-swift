cc9548d1e7c19b4cf34d5ba75ddd1945298c05e3
Chung Tran <bigearsenal@gmail.com>
2019-05-23 11:43:20 +0300

    create message for rx

--- CyberSwift/Managers/EOSManager+Rx.swift
+++ CyberSwift/Managers/EOSManager+Rx.swift
@@ -13,6 +13,67 @@ import eosswift
 extension EOSManager: ReactiveCompatible {}
 
 extension Reactive where Base: EOSManager {
+    // Get chain info
+    static var chainInfo: Single<Info> {
+        return EOSManager.chainApi.getInfo()
+            .flatMap({ (response) -> Single<Info> in
+                guard let body = response.body else {throw ErrorAPI.blockchain(message: "Can not retrieve chain info")}
+                return .just(body)
+            })
+    }
+    
+    //  MARK: - Contract `gls.publish`
+    static func create(message:         String,
+                       headline:        String = "",
+                       parentData:      ParentData? = nil,
+                       tags:            [EOSTransaction.Tags],
+                       jsonMetaData:    String?) -> Single<ChainResponse<TransactionCommitted>> {
+        // Check user authorize
+        guard let userNickName = Config.currentUser.nickName, let userActiveKey = Config.currentUser.activeKey else {
+            return .error(ErrorAPI.invalidData(message: "Unauthorized"))
+        }
+        
+        return chainInfo
+            .flatMap({ (info) -> Single<ChainResponse<TransactionCommitted>> in
+                let messageTransaction = EOSTransaction(chainApi: EOSManager.chainApi)
+                
+                let messageTransactionAuthorizationAbi = TransactionAuthorizationAbi(
+                    actor: AccountNameWriterValue(name: userNickName),
+                    permission: AccountNameWriterValue(name: "active"))
+                
+                let refBlockNum: UInt64 = UInt64(info.head_block_num)
+                
+                let messageCreateArgs = EOSTransaction.MessageCreateArgs(
+                    authorValue:        userNickName,
+                    parentDataValue:    parentData,
+                    refBlockNumValue:   refBlockNum,
+                    headermssgValue:    headline,
+                    bodymssgValue:      message,
+                    tagsValues:         tags,
+                    jsonmetadataValue:  jsonMetaData)
+                
+                // JSON
+                Logger.log(message: messageCreateArgs.convertToJSON(), event: .debug)
+                
+                let messageCreateArgsData = DataWriterValue(hex: messageCreateArgs.toHex())
+                
+                let messageCreateActionAbi = ActionAbi(
+                    account:         AccountNameWriterValue(name:    "gls.publish"),
+                    name:            AccountNameWriterValue(name:    "createmssg"),
+                    authorization:   [messageTransactionAuthorizationAbi],
+                    data:            messageCreateArgsData)
+                
+                let privateKey = try EOSPrivateKey(base58: userActiveKey)
+                
+                return messageTransaction.push(expirationDate: Date.defaultTransactionExpiry(expireSeconds: Config.expireSeconds), actions: [messageCreateActionAbi], authorizingPrivateKey: privateKey)
+                    .map {response -> ChainResponse<TransactionCommitted> in
+                        if !response.success {throw ErrorAPI.blockchain(message: response.errorBody!)}
+                        return response
+                    }
+            })
+    }
+    
+    
     static func delete(messageArgs: EOSTransaction.MessageDeleteArgs) -> Completable {
         guard let userNickName = Config.currentUser.nickName,
             let userActiveKey = Config.currentUser.activeKey else {
--- CyberSwift/Managers/RestAPIManager+Rx.swift
+++ CyberSwift/Managers/RestAPIManager+Rx.swift
@@ -8,10 +8,30 @@
 
 import Foundation
 import RxSwift
+import eosswift
 
 extension RestAPIManager: ReactiveCompatible {}
 
 extension Reactive where Base: RestAPIManager {
+    //  MARK: - Contract `gls.publish`
+    
+    public func create(message:             String,
+                       headline:            String? = "",
+                       parentData:          ParentData? = nil,
+                       tags:                [String]?,
+                       metaData:            String?) -> Single<ChainResponse<TransactionCommitted>> {
+        // Offline mode
+        if (!Config.isNetworkAvailable) { return .error(ErrorAPI.disableInternetConnection(message: nil)) }
+        
+        let arrayTags = tags == nil ? [EOSTransaction.Tags()] : tags!.map({ EOSTransaction.Tags.init(tagValue: $0) })
+        
+        return EOSManager.rx.create(
+            message:          message,
+            headline:         headline ?? String(format: "Test Post Title %i", arc4random_uniform(100)),
+            tags:             arrayTags,
+            jsonMetaData:     metaData)
+    }
+    
     public func deleteMessage(author: String, permlink: String, refBlockNum: UInt64) -> Completable {
         // Offline mode
         if (!Config.isNetworkAvailable) { return .error(ErrorAPI.disableInternetConnection(message: nil)) }
