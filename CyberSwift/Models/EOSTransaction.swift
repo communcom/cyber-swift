c6befc4a07fe3d4cc8e8480556d191aac8d80008
SerhiiM <msm72@mail.ru>
2019-06-05 16:39:53 +0300

    Modify 'EOSTransaction'.

--- CyberSwift/Models/EOSTransaction.swift
+++ CyberSwift/Models/EOSTransaction.swift
@@ -70,10 +70,11 @@ public class EOSTransaction: ChainTransaction {
         let languagemssg: String
         let tags: [Tags]?
         let jsonmetadata: String?
+        let curators_prcnt: UInt16
         
         
         // MARK: - Initialization
-        init(authorValue: String, parentPermlink: String? = nil, beneficiariesValues: [Beneficiary?] = [], tokenpropValue: Int16 = 0, vestpaymentValue: UInt64 = 1, headermssgValue: String = "test", bodymssgValue: String = "test", languagemssgValue: String = "ru", tagsValues: [Tags]? = [Tags()], jsonmetadataValue: String? = "") {
+        init(authorValue: String, parentPermlink: String? = nil, beneficiariesValues: [Beneficiary?] = [], tokenpropValue: Int16 = 0, vestpaymentValue: UInt64 = 1, headermssgValue: String = "test", bodymssgValue: String = "test", languagemssgValue: String = "ru", tagsValues: [Tags]? = [Tags()], jsonmetadataValue: String? = "", curatorsPrcntValue: UInt16 = 0) {
             let prefixTitle         =   parentPermlink == nil ? headermssgValue : "Comment"
             let messagePermlink     =   String.permlinkWith(string: prefixTitle)
             
@@ -87,6 +88,7 @@ public class EOSTransaction: ChainTransaction {
             self.languagemssg       =   languagemssgValue
             self.tags               =   tagsValues
             self.jsonmetadata       =   jsonmetadataValue
+            self.curators_prcnt     =   curatorsPrcntValue
             
 /*
 //            self.parentprmlnk       =   parentprmlnkValue
