bc34fc738c78e446c94ccdd63ac4e70d564d1953
Chung Tran <bigearsenal@gmail.com>
2019-07-31 12:17:38 +0700

    remove deprecated

--- CyberSwift/Extensions/ConstantsApp.swift
+++ CyberSwift/Extensions/ConstantsApp.swift
@@ -61,7 +61,7 @@ public struct Config {
     
     /// Check network connection
     public static var isNetworkAvailable: Bool {
-        return SocketManager.shared.reachability.isReachable
+        return SocketManager.shared.reachability.connection != .none
     }
     
     
