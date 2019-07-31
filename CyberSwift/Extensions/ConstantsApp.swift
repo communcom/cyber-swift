d61112c3acd1801a4c839baa07a0d8525ef8b3d0
Chung Tran <bigearsenal@gmail.com>
2019-07-31 12:49:51 +0700

    switch to 46

--- CyberSwift/Extensions/ConstantsApp.swift
+++ CyberSwift/Extensions/ConstantsApp.swift
@@ -32,7 +32,7 @@ public struct Config {
     public static let expireSeconds: Double = 30.0
     
     #warning("Which blockchain?")
-    public static var isPublicTestnet: Bool             =   true
+    public static var isPublicTestnet: Bool             =   false
     
     static let blockchain_API_Address: String               =   isPublicTestnet ? "http://116.202.4.39"   :   "http://46.4.96.246"
     static let blockchain_API_Port: Int32                   =   8888
