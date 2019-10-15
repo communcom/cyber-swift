a23674e1821efd881f32ac26620510e5e903c430
Chung Tran <bigearsenal@gmail.com>
2019-10-15 12:24:10 +0700

    Unit testing for GetComments

---
+++ CyberSwiftTests/Decodable+MockObjects.swift
@@ -0,0 +1,27 @@
+//
+//  MockObjects.swift
+//  Commun
+//
+//  Created by Chung Tran on 19/04/2019.
+//  Copyright © 2019 Maxim Prigozhenkov. All rights reserved.
+//
+
+import Foundation
+@testable import CyberSwift
+
+extension Decodable {
+    static func mockData() -> Self?  {
+        if let path = Bundle(for: GenerateKeysTests.self).path(forResource: String(describing: self), ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                let result = try JSONDecoder().decode(ResponseAPIResult<Self>.self, from: data)
+//                Logger.log(message: "Retrieved mocking objects for \(String(describing: self)): \(result)", event: .debug)
+                return result.result
+            } catch {
+                print(error)
+                // handle error
+            }
+        }
+        return nil
+    }
+}
---
+++ CyberSwiftTests/ResponseAPIContentGetCommentsTests.swift
@@ -0,0 +1,37 @@
+//
+//  ResponseAPIDecodingTests.swift
+//  CyberSwiftTests
+//
+//  Created by Chung Tran on 10/11/19.
+//  Copyright © 2019 golos.io. All rights reserved.
+//
+
+import XCTest
+@testable import CyberSwift
+
+class ResponseAPIContentGetCommentsTests: XCTestCase {
+    
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testMockData() {
+        let mockData = ResponseAPIContentGetComments.mockData()
+        XCTAssertNotNil(mockData)
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
