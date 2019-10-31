a0abeff1c90bc4fa6f62569741631f99d530c53b
Chung Tran <bigearsenal@gmail.com>
2019-10-31 15:35:17 +0700

    mark document as optional

--- CyberSwift/Models/ResponseAPI/ResponseAPIContent.swift
+++ CyberSwift/Models/ResponseAPI/ResponseAPIContent.swift
@@ -15,7 +15,7 @@ public struct ResponseAPIContentGetPosts: Decodable {
 }
 
 public struct ResponseAPIContentGetPost: Decodable {
-    public var document: ResponseAPIContentBlock
+    public var document: ResponseAPIContentBlock?
     public var votes: ResponseAPIContentVotes
     public let meta: ResponseAPIContentMeta
     public let contentId: ResponseAPIContentId
