91d776b45a5959a1e0e0974efbc92dea748f0cee
Sergey Monasturskiy <msm72@mail.ru>
2019-12-03 10:41:09 +0200

    Modify UILabel extensions #421.

--- CyberSwift/Extensions/UILabel+Extensions.swift
+++ CyberSwift/Extensions/UILabel+Extensions.swift
@@ -24,23 +24,30 @@ extension UILabel {
         self.textAlignment      =   alignment
     }
     
-    public func tune(withAttributedText text: String,
-                     hexColors: ThemeColorPicker?,
-                     font: UIFont?,
-                     alignment: NSTextAlignment,
-                     isMultiLines: Bool,
-                     lineSpacing: CGFloat = 1.3,
-                     lineHeight: CGFloat = 26) {
+    public func tune(withAttributedText text:   String,
+                     hexColors:                 ThemeColorPicker?,
+                     font:                      UIFont?,
+                     alignment:                 NSTextAlignment,
+                     isMultiLines:              Bool,
+                     lineHeight:                CGFloat = 26.0,
+                     lineSpacing:               CGFloat = 0.0,
+                     baselineOffset:            CGFloat = 0.0,
+                     lineHeightMultiple:        CGFloat = 1.0) {
         ThemeManager.setTheme(index: Config.isAppThemeDark ? 1 : 0)
         
-        let attributedString    =   NSMutableAttributedString(string:      text.localized(),
+        let attributedString    =   NSMutableAttributedString(string:      text.localized().uppercaseFirst,
                                                               attributes:  [ NSAttributedString.Key.font: font! ])
         
         let style               =   NSMutableParagraphStyle()
-        style.lineSpacing       =   lineSpacing * Config.widthRatio
-        style.minimumLineHeight =   lineHeight * Config.widthRatio
+        style.lineSpacing       =   CGFloat.adaptive(height: lineSpacing)
+        style.minimumLineHeight =   CGFloat.adaptive(height: lineHeight)
         
-        attributedString.addAttributes([NSAttributedString.Key.paragraphStyle: style], range: NSRange(location: 0, length: text.localized().count))
+        if lineHeightMultiple != 1.0 {
+            style.lineHeightMultiple = CGFloat.adaptive(height: lineHeightMultiple)
+        }
+        
+        attributedString.addAttributes([NSAttributedString.Key.paragraphStyle: style,
+                                        .baselineOffset: baselineOffset], range: NSRange(location: 0, length: text.localized().count))
         self.attributedText     =   attributedString
         
         self.font               =   font
