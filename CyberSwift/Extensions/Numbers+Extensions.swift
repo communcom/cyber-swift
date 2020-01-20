f530f9b3e3b342790be50779e7d9d3958a3e0453
Sergey Monasturskiy <msm72@mail.ru>
2020-01-20 11:02:18 +0200

    Modify Numbers extension.

--- CyberSwift/Extensions/Numbers+Extensions.swift
+++ CyberSwift/Extensions/Numbers+Extensions.swift
@@ -16,10 +16,18 @@ extension CGFloat {
     public static func adaptive(height: CGFloat) -> CGFloat {
         return height * Config.heightRatio
     }
+    
+    public func convertToString(withAccuracy accuracy: Int) -> String {
+        switch self {
+        case 0:
+            return String(format: "0.%i", Int(Double(self.fraction) * pow(10.0, Double(accuracy))))
 
-    public var formattedWithSeparator: String {
-        guard self >= 1_000 else { return "\(self)" }
-        return Formatter.withSeparator.string(for: self) ?? ""
+        case 0..<1000:
+            return String(format: "%i.%i", Int(self), Int(Double(self.fraction) * pow(10.0, Double(accuracy))))
+
+        default:
+            return String(format: "%@.%i", self.whole.formattedWithSeparator, Int(Double(self.fraction) * pow(10.0, Double(accuracy))))
+        }
     }
 }
 
@@ -34,6 +42,16 @@ extension Formatter {
 
 extension UInt64 {
     public var formattedWithSeparator: String {
+        return Formatter.withSeparator.string(for: self) ?? ""
+    }
+}
+
+extension FloatingPoint {
+    public var whole: Self { modf(self).0 }
+    public var fraction: Self { modf(self).1 }
+    
+    public var formattedWithSeparator: String {
+        guard self >= 1_000 else { return "\(self)" }
         return Formatter.withSeparator.string(for: self) ?? ""
     }
 }
