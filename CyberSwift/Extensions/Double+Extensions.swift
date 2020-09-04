17884f55665ea42a6a90755a21512e3e0901259f
Chung Tran <bigearsenal@gmail.com>
2020-09-04 10:24:59 +0700

    add convenience methods

--- CyberSwift/Extensions/Double+Extensions.swift
+++ CyberSwift/Extensions/Double+Extensions.swift
@@ -16,4 +16,22 @@ extension Double {
         formatter.maximumFractionDigits = (self < 1000) ? 4 : 2
         return formatter.string(from: self as NSNumber) ?? "0"
     }
+    
+    public var currencyValueFormatted: String {
+        let formatter = NumberFormatter()
+        formatter.groupingSize = 3
+        formatter.numberStyle = .decimal
+        formatter.usesGroupingSeparator = true
+        formatter.locale = Locale(identifier: "en")
+
+        if self > 1000 {
+            formatter.maximumFractionDigits = 2
+        } else if self < 100 {
+            formatter.maximumFractionDigits = 3
+        } else {
+            formatter.maximumFractionDigits = 2
+        }
+        
+        return (formatter.string(from: self as NSNumber) ?? "0").replacingOccurrences(of: ",", with: " ")
+    }
 }
